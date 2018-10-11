4e30130eef893a1f4a7f6677a48e8dae05614e15
Manuel Weichselbaumer <mincequi@web.de>
2018-10-11 23:23:32 +0200

    Remove gstreamermm

--- thirdparty/gstreamermm-1.0-1.10.0+dfsg+bt/autogen.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh -e
-test -n "$srcdir" || srcdir=`dirname "$0"`
-test -n "$srcdir" || srcdir=.
-
-mm-common-prepare --force --copy "$srcdir"
-touch gstreamer/src/plugin_filelist.am
-autoreconf --force --install --verbose "$srcdir"
-test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
