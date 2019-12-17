d8624b00dee7d928df5b8bce2bdc48f5fbd71e95
Manuel Weichselbaumer <Manuel.Weichselbaumer@bmw.de>
2019-12-17 06:32:50 +0100

    Use coro AlsaSink for sqream

--- tests/sqreamtest.cpp
+++ tests/sqreamtest.cpp
@@ -1,3 +1,6 @@
+#define private public
+#include <coro/audio/AlsaSink.h>
+#undef private
 #include <coro/audio/AppSource.h>
 #include <coro/audio/SbcDecoder.h>
 
@@ -7,6 +10,29 @@
 #include <QUdpSocket>
 
 using namespace coro::audio;
+
+int main2(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    AudioBuffer buffer(3+5+1152);
+    AlsaSink sink;
+
+    QUdpSocket socket;
+    socket.bind(QHostAddress::AnyIPv4, 4010, QUdpSocket::ShareAddress);
+    socket.joinMulticastGroup(QHostAddress("239.255.77.77"));
+    QObject::connect(&socket, &QUdpSocket::readyRead, [&]() {
+        while (socket.hasPendingDatagrams()) {
+            auto data = buffer.acquire(3+5+1152);
+            assert(socket.readDatagram(data+3, 5+1152) == 5+1152);
+            buffer.commit(3+5+1152);
+
+            sink.process({ AudioCodec::RawInt16, SampleRate::Rate44100, Channels::Stereo }, buffer);
+        }
+    });
+
+    return a.exec();
+}
 
 int main(int argc, char *argv[])
 {
